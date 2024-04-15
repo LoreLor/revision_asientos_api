16d7155fc8b1283e9d75cd51075a386a8546ad16
LoreLor <lorenadearmas01@gmail.com>
2024-04-15 08:48:10 -0300

    configuración entities

--- src/main/java/com/ayigroup/revision_asientos/entities/Asiento.java
+++ src/main/java/com/ayigroup/revision_asientos/entities/Asiento.java
@@ -4,6 +4,8 @@ import lombok.*;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -17,14 +19,47 @@ public class Asiento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotNull
     private Integer documento;
+
+    @NotNull
+    @Size(max=50)
     private String tipoDocumento;
+
+    @NotNull
+    @Size(max=100)
     private String compania;
+
+    @NotNull
+    @Size(max=100)
     private String unidadDeNegocio;
+
+    @NotNull
+    @Size(max=100)
     private String cuentaObjeto;
+
+    @NotNull
+    @Size(max=100)
     private String cuentaAuxiliar;
+
+    @NotNull
+    @Size(max=50)
     private String libro;
+
+
     private Integer lieneaAsiento;
+
+    @NotNull
     private Date fechaContable;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoProceso estadoProceso;
+
+    public enum EstadoProceso {
+        PENDIENTE,
+        PROCESADO,
+        RECHAZADO
+    }
 
 }
